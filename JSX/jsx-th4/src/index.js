a3d30f7d9f6fb735d92043a61fc88420934d9823
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-08 23:51:53 +0700

    jsx - thuc hanh 4- hien thi thoi gian

--- JSX/jsx-th4/src/index.js
+++ JSX/jsx-th4/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// thực hành 4: Hiển thị thời gian hiện tại (dd/mm/yyyy – hh/mm/ss) bằng JSX
+const tick = (  
+ 
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+     
+    );
+    
+  
+    setInterval(tick, 1000);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(tick);
