7dbfa0d16bdb16cc3897adbd1ec443212ed966b3
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-08 20:29:04 +0700

    jsx - thuc hanh 2- nhung ten cua ban vao react element bang jsx

--- JSX/jsx-th2/src/index.js
+++ JSX/jsx-th2/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './index.css';
-// import App from './App';
-import  {createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 // thực hành 2: Nhúng tên của bạn vào React Element tạo bởi JSX
 const myname = "Lê Ngọc Bảo"
 
 const app = (
-  <><h1>TH2:</h1><h1 style={{ textAlign: "center" }}>
-    {myname}
-  </h1></>
+    <><h1>TH2:</h1><h1 style={{ textAlign: "center" }}>
+        {myname}
+    </h1></>
 );
 const container = document.getElementById('root');
 const root = createRoot(container);
