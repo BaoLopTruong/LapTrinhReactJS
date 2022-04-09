1b61ee473fedacd6a6a92037239585d2fade8e00
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-09 15:34:30 +0700

    jsx - bai tap 1: Tao Element

--- JSX/jsx-bt1/src/index.js
+++ JSX/jsx-bt1/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// bai tap 1: Tạo Element thể hiện thông tin của Trình duyệt bạn đang sử dụng
+
+const div = (
+  <h4>Browser's details: { navigator.userAgent }</h4>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(div);
