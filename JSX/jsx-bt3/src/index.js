a93e12bf8de7c987a1ae7b0466e95feb74aff154
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-09 16:06:31 +0700

    jsx - bai tap 3: Tao Element hien thi profile card

--- JSX/jsx-bt3/src/index.js
+++ JSX/jsx-bt3/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const info=
+//   {
+//     name: "Le Ngoc Bao",
+//     avatar: "https://scontent.fsgn8-2.fna.fbcdn.net/v/t39.30808-6/193328816_4380531752005148_3865120816637615726_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=mWqBecExsn0AX_FhFlS&_nc_oc=AQkloQTvGaMS6MvO2-LkvzfWhKhMfO7fexwPI8aMTuRpxWSp-gPldZtgr9U8G191ap8&_nc_ht=scontent.fsgn8-2.fna&oh=00_AT-eDFM4nNBxnZEXyuyGokH_9GBXxMcCuUGRk_Bv3g4OyQ&oe=62571502",
+//     retail: " Once day is coder, Always is coder"
+//   }
+
+//bai tap 3: Tạo Element thể hiện Profile Card
+
+const show =(
+  <div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img className="avatar" src="https://scontent.fsgn8-2.fna.fbcdn.net/v/t39.30808-6/193328816_4380531752005148_3865120816637615726_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=mWqBecExsn0AX_FhFlS&_nc_oc=AQkloQTvGaMS6MvO2-LkvzfWhKhMfO7fexwPI8aMTuRpxWSp-gPldZtgr9U8G191ap8&_nc_ht=scontent.fsgn8-2.fna&oh=00_AT-eDFM4nNBxnZEXyuyGokH_9GBXxMcCuUGRk_Bv3g4OyQ&oe=62571502" alt="avatar" />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Le Ngoc Bao</p>
+          <p className="text-sub">
+          Once day is coder, Always is coder
+          </p>
+          <button className="btn third" >FOLLOW</button>
+        </div>
+      </div>
+    </div>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(show);
