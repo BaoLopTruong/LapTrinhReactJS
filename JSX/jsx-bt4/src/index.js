910ae24b13bc4f450a51a653ef602aa020a7c3ac
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-09 16:26:27 +0700

    jsx - bai tap 4: Tao Element hien thi form dang nhap

--- JSX/jsx-bt4/src/index.js
+++ JSX/jsx-bt4/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css'; 
+// bai tap 4: Tạo Element thể hiện Sign in Form sử dụng Bootstrap
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const show =(
+  <div className="container d-flex align-items-center text-center">
+  <div className="form-signin">
+    <form>
+      <img className="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="" width="72" height="57" />
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <div className="form-floating">
+        <input type="email" className="form-control email" id="floatingInput" placeholder="name@example.com" />
+        <label>Email address</label>
+      </div>
+      <div className="form-floating">
+        <input type="password" className="form-control password" id="floatingPassword" placeholder="Password" />
+        <label>Password</label>
+      </div>
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" /> Remember me
+        </label>
+      </div>
+      <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p className="mt-5 mb-3 text-muted">© 2017-2021</p>
+    </form>
+  </div>
+</div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(show);
