a82ff54db090f37ac64d24737228b3b756628c86
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-12 21:08:09 +0700

    React Component-bt1-Xay dung alert component

---
+++ react-component/baitap/rc-bt1/src/App.js
@@ -0,0 +1,12 @@
+import Alert from './components/Alert';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-component/baitap/rc-bt1/src/components/Alert.js
@@ -0,0 +1,9 @@
+function Alert(props) {
+    return (
+        <div class="alert alert-warning" role="alert">
+  <h1> { props.text} </h1>
+      </div>
+    );
+}
+ 
+export default Alert;
