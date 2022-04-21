7295fc8a2240c4f30b59e566dc49118699bf5af0
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-22 06:48:19 +0700

    state-th2- thay doi mau nen bang setTimeout

--- state-reactjs/thuchanh/state-th2/src/App.js
+++ state-reactjs/thuchanh/state-th2/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import  { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'black',
+    };
+  }
+ 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ color: 'pink' });
+    }, 5000);
+  }
+ 
+  render() {
+    return (
+      <div>
+      <h1 style={{textAlign: "center"}}>Thay đổi màu nền backgroundColor bằng setTimeout</h1>
+        <div
+          style={{
+            backgroundColor: this.state.color,
+            paddingTop: 20,
+            width: 400,
+            height: 80,
+            margin: 'auto'
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
