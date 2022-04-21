f994a7f8fd48e5aad2baf60b46d10a1080981fcc
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-22 06:38:16 +0700

    state-th1- dem so lan click vao button

--- state-reactjs/thuchanh/state-th1/src/App.js
+++ state-reactjs/thuchanh/state-th1/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import  { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+ 
+  increment = () => {
+    this.setState({ number: this.state.number + 1 });
+  }
+  decrease = () => {
+    this.setState({ number: this.state.number - 1 });
+  }
+ 
+  render() {
+    return (
+      <div style={{textAlign: "center", padding: 30}}>
+      <h1>Đếm số lần click vào button</h1>
+        <button onClick={this.increment}>Increment</button>
+        <span style={{ padding: 10 }}>{ this.state.number }</span>
+        <button onClick={this.decrease}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
