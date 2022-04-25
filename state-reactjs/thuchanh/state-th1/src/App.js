8e3784747c1f909018c659d242455ee36bbba7f1
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-25 19:47:42 +0700

    dem so lan click

--- state-reactjs/thuchanh/state-th1/src/App.js
+++ state-reactjs/thuchanh/state-th1/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import  { Component } from 'react';
+import { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -8,20 +8,20 @@ class App extends Component {
       number: 0,
     };
   }
- 
+
   increment = () => {
     this.setState({ number: this.state.number + 1 });
   }
   decrease = () => {
     this.setState({ number: this.state.number - 1 });
   }
- 
+
   render() {
     return (
-      <div style={{textAlign: "center", padding: 30}}>
-      <h1>Đếm số lần click vào button</h1>
+      <div style={{ textAlign: "center", padding: 30 }}>
+        <h1>Đếm số lần click vào button</h1>
         <button onClick={this.increment}>Increment</button>
-        <span style={{ padding: 10 }}>{ this.state.number }</span>
+        <span style={{ padding: 10 }}>{this.state.number}</span>
         <button onClick={this.decrease}>Decrease</button>
       </div>
     );
