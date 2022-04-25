a692b18e515ac76b213b8bf6abccb1478ef5dbc7
BaoLopTruong <ngocbaole1999@gmail.com>
2022-04-25 20:28:33 +0700

    thay doi mau nen

--- state-reactjs/thuchanh/state-th2/src/App.js
+++ state-reactjs/thuchanh/state-th2/src/App.js
@@ -1,25 +1,25 @@
 
 import './App.css';
-import  { Component } from 'react';
+import { Component } from 'react';
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state= {
+    this.state = {
       color: "black"
     }
   }
-componentDidMount(){
-  console.log("compoentdidmount")
-  setTimeout(() => {
-    this.setState({color: "pink"});
-    
-  }, 3000);
-}
+  componentDidMount() {
+    console.log("compoentdidmount")
+    setTimeout(() => {
+      this.setState({ color: "pink" });
 
-  render(){
+    }, 3000);
+  }
+
+  render() {
     console.log("render")
-    return(
-      <div style={{backgroundColor:this.state.color}}>
+    return (
+      <div style={{ backgroundColor: this.state.color }}>
         <h3>  hello world</h3>
       </div>
     )
