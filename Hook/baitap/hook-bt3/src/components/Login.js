461276f5f6164aa3d92f684cfb0dfb64be5e63a0
BaoLopTruong <ngocbaole1999@gmail.com>
2022-05-02 22:42:06 +0700

    hook - bt3

--- Hook/baitap/hook-bt3/src/components/Login.js
+++ Hook/baitap/hook-bt3/src/components/Login.js
@@ -5,31 +5,29 @@ export default function Login() {
     const [password, setPassword] = useState('456');
 
     return (
-        <div>
+        <div  style={{textAlign: 'center'}}>
             {/* <h1> Login Account</h1>
             <input type={'text'} onChange={(e) =>{setUserName(e.target.value)}} ></input>
             <input type={'password'} onChange={(e) =>{setPassword(e.target.value)}}></input>
             <button onClick={() =>{ alert("username:"+username + ", password: " + password)}}>Login</button> */}
+            <h1>Login</h1>
             <form>
 
                 <div className="form-outline mb-3">
-                    <input type="email" id="form2Example1" className="form-control"   
-                    onChange={(e) =>{setUserName(e.target.value)}}/>
+                    <input type="email" id="form2Example1" className=""   
+                    onChange={(e) =>{setUserName(e.target.value)}}/> <br></br>
                     <label className="form-label" >Email address</label>
                 </div>
 
 
                 <div className="form-outline mb-3">
-                    <input type="password" id="form2Example2" className="form-control"  
-                    onChange={(e) =>{setPassword(e.target.value)}}/>
+                    <input type="password" id="form2Example2" className=""  
+                    onChange={(e) =>{setPassword(e.target.value)}}/> <br></br>
                     <label className="form-label" >Password</label>
                 </div>
 
                 <button type="button" className="btn btn-primary btn-block mb-4" 
                 onClick={() =>{ alert("username:"+username + ", password: " + password)}}>Sign in</button>
-
-
-
 
             </form>
 
