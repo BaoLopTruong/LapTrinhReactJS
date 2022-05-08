d5245d647c8f59a3d211f21bf19a7081d268d0fd
BaoLopTruong <ngocbaole1999@gmail.com>
2022-05-08 21:40:52 +0700

    react-form-bt1

--- react-form/baitap/form-bt1/src/App.js
+++ react-form/baitap/form-bt1/src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Formik } from "formik";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const REGEX = {
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+  };
+
+  const [form, setForm] = useState({});
+
+  function handleChange(event) {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function handleValidate() {
+    const errors = {};
+    if (!form.email) {
+      errors.email = "Required";
+    } else if (!REGEX.email.test(form.email)) {
+      errors.email = "Invalid email address";
+      console.log("code");
+    }
+    if (!form.name) {
+      errors.name = "Required";
+    }
+    if (!form.phone) {
+      errors.phone = "Required";
+    }
+    return errors;
+  }
+
+  function handleSubmit() {
+    alert("Login in successfully!!!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sign up</h1>
+      <Formik
+        initialValues={form}
+        validate={handleValidate}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div
+              className={`custom-input ${
+                errors.name ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                value={form.name || ""}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.name}</p>
+            </div>
+            
+            <div
+              className={`custom-input ${
+                errors.email ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                value={form.email || ""}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.email}</p>
+            </div>
+
+            <div
+              className={`custom-input ${
+                errors.phone ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Phone</label>
+              <input
+                type="number"
+                name="phone"
+                value={form.phone || ""}
+                onChange={handleChange}
+              />
+              <p className="error">{errors.phone}</p>
+            </div>
+            <div
+              className={`custom-input ${
+                errors.message ? "custom-input-error" : ""
+              }`}
+            >
+              <label>Message</label>
+              {/* <input
+                type="textArea"
+                name="message"
+                value={form.message || ""}
+                onChange={handleChange}
+              /> */}
+              <textarea
+               name="message"
+                value={form.message || ""}
+                onChange={handleChange}
+              ></textarea>
+              <p className="error">{errors.message}</p>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
-
-export default App;
