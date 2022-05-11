e752119c45ec579eaf5dbab83bcf1957b762dfec
BaoLopTruong <ngocbaole1999@gmail.com>
2022-05-11 08:23:46 +0700

    react-router -th1

---
+++ react-router/thuchanh/router-th1/src/App.js
@@ -0,0 +1,33 @@
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ react-router/thuchanh/router-th1/src/components/Home.js
@@ -0,0 +1,6 @@
+function Home() {
+    return (
+        <h1>Home Page</h1>
+    );
+}
+export default Home;
