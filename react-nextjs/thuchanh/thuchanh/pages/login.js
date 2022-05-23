2d86a1635c53234f6031c7df5753212dbefcb643
BaoLopTruong <ngocbaole1999@gmail.com>
2022-05-23 19:49:20 +0700

    update

--- react-nextjs/thuchanh/thuchanh/pages/login.js
+++ react-nextjs/thuchanh/thuchanh/pages/login.js
@@ -4,13 +4,13 @@ import styles from '../styles/Login.module.css'
 export default function Login() {
   const router = useRouter()
   return (
-    <div class={styles.wrapper}>
-      <div class={styles.formContent}>
+    <div className={styles.wrapper}>
+      <div className={styles.formContent}>
         <h2>Login</h2>
         <form>
-          <input class={styles.input} type="text" id="login" name="login" placeholder="login" />
-          <input class={styles.input} type="text" id="password" name="login" placeholder="password" />
-          <button type="button" class={styles.button} onClick={() => router.push('/')}>Log In</button>
+          <input className={styles.input} type="text" id="login" name="login" placeholder="login" />
+          <input className={styles.input} type="text" id="password" name="login" placeholder="password" />
+          <button type="button" className={styles.button} onClick={() => router.push('/home')}>Log In</button>
         </form>
       </div>
     </div>
