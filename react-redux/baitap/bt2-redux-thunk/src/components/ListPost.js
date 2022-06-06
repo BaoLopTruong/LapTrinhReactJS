fc1d3712beafdedb2e4fcec57a54eeb7efca2c34
BaoLopTruong <ngocbaole1999@gmail.com>
2022-06-06 19:36:54 +0700

    bt2-redux-thunk-create-components

---
+++ react-redux/baitap/bt2-redux-thunk/src/components/ListPost.js
@@ -0,0 +1,13 @@
+
+
+export default function ListPost(){
+
+    return(
+        <div className="list-post">
+            <div className="post">
+                <h3> Title</h3>
+                <p>Content</p>
+            </div>
+        </div>
+    )
+}
